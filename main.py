d5ee698f23f629451a69149c0f5107f4ba2e506b
git-ankitgautam <ankitgautam485@gmail.com>
2024-10-10 17:57:29 +0530

    implemented deletion of files after report has been generated

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ import streamlit as st
 from war_reports_generator import main
 import os
 
+st.set_page_config(page_title="RW Report Generator")
+def cleanup():
+    try:
+        os.remove(f"{filename}.xlsx")
+        os.remove(f"{filename}.attacks.txt")
+        os.remove(f"{filename}.warData.txt")
+    except:
+        pass
+
 def update_progress(current_step, total_steps):
     progress = (current_step / total_steps)
     progress_bar.progress(progress)
 
 def update_stage(stage_message):
     stage_text.text(stage_message)
+
 
 st.title("RW Report Generator")
 #make two columns to show the text boxes
@@ -21,13 +31,9 @@ filename = column2.text_input("Filename:")
 
 if st.button("Generate Report"):
     progress_bar = st.progress(0)
-    stage_text = st.text("") 
-    main(faction_id, war_id, str(leader_api_key),str(filename),progress_callback=update_progress, stage_callback=update_stage)
+    stage_text = st.text("")
+    #main(faction_id, war_id, str(leader_api_key),str(filename),progress_callback=update_progress, stage_callback=update_stage)
     with open(f"{filename}.xlsx", 'rb') as excel_file:
-        excel_file_downloaded = st.download_button("Download report file",data=excel_file, file_name=filename + ".xlsx")
-
-
-if excel_file_downloaded:
-    os.remove(f"{filename}.attacks.txt")
-    os.remove(f"{filename}.warData.txt")
-    os.remove(f"{filename}.xlsx")
+        pass
+    st.download_button("Download report file",data=excel_file, file_name=filename + ".xlsx", on_click=cleanup())
+    excel_file.close()
